ddde45993ff7f7bc853f7e52ce1f40df71cdb13c
roshanrudroju <roshanrudroju@outlook.com>
2025-02-04 19:16:27 -0500

    Add files via upload

---
+++ Message.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "roshanrudroju@outlook.com";
+    $subject = "New Contact Form Message";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message sending failed.";
+    }
+}
+?>
